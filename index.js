d9a361f46054ee4730ac6493db0d757c86ad17fd
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-08-17 04:22:07 -0700

    assighnment 18

---
+++ index.js
@@ -0,0 +1,18 @@
+//problem 1
+function square (a){
+    var square = a * a;
+    return square
+}
+var squareOfNmbr = (4)
+console.log(`Square ofgiven Numbr: ${squareOfNmbr}`)
+
+//problem 2
+var count = 10 ;
+ function myFunction(){
+    var count = 15;
+count= count * 2;
+return count
+ } 
+ var square = myFunction();
+  console.log(`global count: ${count}`)
+  console.log(`local count : ${square}`)
